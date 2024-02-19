a486c75507a2aac9e3b719fb16d60dc383838e03
ricardosilva <ricardo@we-rtek.com>
2024-02-19 10:42:38 -0300

    add solar data extract

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,10 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-import { database } from './firebase';
-import { get, ref, set } from 'firebase/database';
 //import * as moment from 'moment-timezone';
 import { PowerData } from './models/power-data/power-data.class';
 import { InfluxDB, Point } from '@influxdata/influxdb-client';
 import { EventGateway } from './gateway/event/event.gateway';
+import * as puppeteer from 'puppeteer';
 
 @Controller()
 export class AppController {
@@ -166,117 +165,61 @@ export class AppController {
     writeClient.writePoint(point);
     await writeClient.flush();
 
+    this.extractSolarData();
+
     this.eventGateway.server.emit('events', 'Hello from the server');
-    // //Obtem ultima leitura armazenda
-    // const now = await get(ref(database, 'user_identity/now'));
-    // let last_epa_c = 0;
-    // let last_epb_c = 0;
-    // let last_epc_c = 0;
-    // let last_ept_c = 0;
-    // let last_epa_g = 0;
-    // let last_epb_g = 0;
-    // let last_epc_g = 0;
-    // let last_ept_g = 0;
-    // dataF.kwhAPer = 0;
-    // dataF.kwhBPer = 0;
-    // dataF.kwhCPer = 0;
-    // dataF.kwhTPer = 0;
-    // dataF.kwhAGerPer = 0;
-    // dataF.kwhBGerPer = 0;
-    // dataF.kwhCGerPer = 0;
-    // dataF.kwhTGerPer = 0;
-    // if (now.exists()) {
-    //   last_epa_c = now.val().epa_c;
-    //   last_epb_c = now.val().epb_c;
-    //   last_epc_c = now.val().epc_c;
-    //   last_ept_c = now.val().ept_c;
-    //   last_epa_g = now.val().epa_g;
-    //   last_epb_g = now.val().epb_g;
-    //   last_epc_g = now.val().epc_g;
-    //   last_ept_g = now.val().ept_g;
-    //   dataF.kwhAPer = dataF.epa_c - last_epa_c;
-    //   dataF.kwhBPer = dataF.epb_c - last_epb_c;
-    //   dataF.kwhCPer = dataF.epc_c - last_epc_c;
-    //   dataF.kwhTPer = dataF.ept_c - last_ept_c;
-    //   dataF.kwhAGerPer = dataF.epa_g - last_epa_g;
-    //   dataF.kwhBGerPer = dataF.epb_g - last_epb_g;
-    //   dataF.kwhCGerPer = dataF.epc_g - last_epc_g;
-    //   dataF.kwhTGerPer = dataF.ept_g - last_ept_g;
-    // }
-
-    // const lastSum = await get(
-    //   ref(database, `user_identity/${year}/${month}/${day}/sum`),
-    // );
-    // if (lastSum.exists()) {
-    //   dataF.kwhAConAcum = lastSum.val().kwhAConAcum + dataF.kwhAPer;
-    //   dataF.kwhBConAcum = lastSum.val().kwhBConAcum + dataF.kwhBPer;
-    //   dataF.kwhCConAcum = lastSum.val().kwhCConAcum + dataF.kwhCPer;
-    //   dataF.kwhTConAcum = lastSum.val().kwhTConAcum + dataF.kwhTPer;
-    //   dataF.kwhAGerAcum = lastSum.val().kwhAGerAcum + dataF.kwhAGerPer;
-    //   dataF.kwhBGerAcum = lastSum.val().kwhBGerAcum + dataF.kwhBGerPer;
-    //   dataF.kwhCGerAcum = lastSum.val().kwhCGerAcum + dataF.kwhCGerPer;
-    //   dataF.kwhTGerAcum = lastSum.val().kwhTGerAcum + dataF.kwhTGerPer;
-    // } else {
-    //   dataF.kwhAConAcum = dataF.kwhAPer;
-    //   dataF.kwhBConAcum = dataF.kwhBPer;
-    //   dataF.kwhCConAcum = dataF.kwhCPer;
-    //   dataF.kwhTConAcum = dataF.kwhTPer;
-    //   dataF.kwhAGerAcum = dataF.kwhAGerPer;
-    //   dataF.kwhBGerAcum = dataF.kwhBGerPer;
-    //   dataF.kwhCGerAcum = dataF.kwhCGerPer;
-    //   dataF.kwhTGerAcum = dataF.kwhTGerPer;
-    // }
-
-    // set(
-    //   ref(
-    //     database,
-    //     `user_identity/${year}/${month}/${day}/${hour}/${min}/${sec}`,
-    //   ),
-    //   dataF,
-    // );
-    // set(ref(database, 'user_identity/now'), dataF);
-
-    // // UPDATE UPPER LEVEL WITH SUM OF ALL DATA
-    // this.updateParentSum(
-    //   `user_identity/${year}/${month}/${day}/${hour}/${min}/sum`,
-    //   dataF,
-    // );
-    // this.updateParentSum(
-    //   `user_identity/${year}/${month}/${day}/${hour}/sum`,
-    //   dataF,
-    // );
-    // this.updateParentSum(`user_identity/${year}/${month}/${day}/sum`, dataF);
-    // this.updateParentSum(`user_identity/${year}/${month}/sum`, dataF);
-    // this.updateParentSum(`user_identity/${year}/sum`, dataF);
   }
 
-  private async updateParentSum(sumPath: string, dataF: PowerData) {
-    get(ref(database, sumPath)).then((snapshot) => {
-      if (snapshot.exists()) {
-        let data = snapshot.val();
-        data = { ...data, ...dataF };
-        data.kwhAConAcum += dataF.kwhAPer;
-        data.kwhBConAcum += dataF.kwhBPer;
-        data.kwhCConAcum += dataF.kwhCPer;
-        data.kwhTConAcum += dataF.kwhTPer;
-        data.kwhAGerAcum += dataF.kwhAGerPer;
-        data.kwhBGerAcum += dataF.kwhBGerPer;
-        data.kwhCGerAcum += dataF.kwhCGerPer;
-        data.kwhTGerAcum += dataF.kwhTGerPer;
-        set(ref(database, sumPath), data);
-      } else {
-        set(ref(database, sumPath), {
-          ...dataF,
-          kwhAConAcum: dataF.kwhAPer,
-          kwhBConAcum: dataF.kwhBPer,
-          kwhCConAcum: dataF.kwhCPer,
-          kwhTConAcum: dataF.kwhTPer,
-          kwhAGerAcum: dataF.kwhAGerPer,
-          kwhBGerAcum: dataF.kwhBGerPer,
-          kwhCGerAcum: dataF.kwhCGerPer,
-          kwhTGerAcum: dataF.kwhTGerPer,
-        });
-      }
-    });
+  private async extractSolarData() {
+    try {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+
+      await page.goto('http://solar-monitoramento.intelbras.com.br/login');
+
+      await page.type('#val_loginAccount', 'ricardo.ara.silva@gmail.com');
+      await page.type('#val_loginPwd', '234567');
+
+      await Promise.all([
+        page.waitForNavigation(),
+        page.waitForSelector('.val_eToday'),
+        page.waitForRequest(
+          'http://solar-monitoramento.intelbras.com.br/panel/intelbras/getInvDayChart',
+        ),
+        page.waitForNetworkIdle(),
+        page.click('.loginB'),
+      ]);
+
+      const data = await page.evaluate(() => {
+        const dataElement = document.querySelector('.val_eToday');
+        return dataElement?.textContent ?? null;
+      });
+
+      console.log(`Extracted data: ${data}`);
+
+      await browser.close();
+
+      const token =
+        'ZgzULOOA4gARxR7mxs3qGEwpC_rUzZkunLaxPTcA6iTl1yWpu0Mob_CYxHKLCAFqUyZE8WfcjAnY9c73St_9Kg==';
+      const url = 'http://52.40.160.15:8086';
+
+      const client = new InfluxDB({ url, token });
+
+      //WRITE
+
+      const org = `wertek`;
+      const bucket = `solarmonitor`;
+
+      const writeClient = client.getWriteApi(org, bucket, 'ns');
+
+      const point = new Point('solardata')
+        .tag('user', 'user_identity')
+        .floatField('total', data);
+
+      writeClient.writePoint(point);
+      await writeClient.flush();
+    } catch (error) {
+      console.error('Error extracting solar data', error);
+    }
   }
 }
